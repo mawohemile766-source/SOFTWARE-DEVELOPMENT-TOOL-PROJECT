00a9af8f4ae3855106d39cd179262f7b9a757149
osval-27 <oswaldkarisma27@gmail.com>
2025-12-05 09:10:31 -0700

    updated the client ID hehehe!!

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const { OAuth2Client} =  require("google-auth-library");
 const app = express()
 app.use(express.json());
 app.use(cors());
-const client = new OAuth2Client("YOUR_GOOGLE_CLIENT_ID");
+const client = new OAuth2Client("172963971660-44jiecvlnp6msgfpeu096nd3a8sh05rh.apps.googleusercontent.com");
 
-app.post("ggogle- login", async (req, res) =>{
+app.post("google- login", async (req, res) =>{
     const { idToken} = req.body;
     try {
         const ticket = await client.verifyIdToken({
             idToken,
-            audience: "YOUR_GOOGLE_CLIENT_ID"
+            audience: "172963971660-44jiecvlnp6msgfpeu096nd3a8sh05rh.apps.googleusercontent.com"
             });
             const payload = ticket.getPayload();
             res.json({
